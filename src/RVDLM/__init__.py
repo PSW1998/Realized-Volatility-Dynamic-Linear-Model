c35d0ca24f8aa8b8bfb5940becd6dc83cd5fc0ef
PSW1998 <59308806+PSW1998@users.noreply.github.com>
2025-11-21 10:11:06 -0500

    Update __init__.py

--- src/RVDLM/__init__.py
+++ src/RVDLM/__init__.py
@@ -1,20 +1,5 @@
 # src/RVDLM/__init__.py
 
-"""
-RVDLM: Realized Volatility Dynamic Linear Models
-
-Reference implementation for:
-
-  "Bayesian dynamic modelling of realized volatility in financial asset return forecasting"
-  by Patrick Woitschig and Mike West.
-
-Provides:
-- DynamicGammaFilter: dynamic-F variance block (Sec. Dynamic Gamma Model)
-- RVDLM_Univariate: price + realized-variance RV–DLM (Secs. 3–4)
-- Quarticity-based α_t construction (Sec. Quarticity–matched effective DOF)
-- Baseline DLM and scoring utilities for empirical comparisons.
-"""
-
 from importlib.metadata import version, PackageNotFoundError
 
 try:
@@ -22,17 +7,20 @@ try:
 except PackageNotFoundError:
     __version__ = "0.0.0"
 
-from .dynamic_gamma import DynamicGammaFilter
-from .rvdlm_univariate import RVDLM_Univariate
-from .quarticity import alpha_from_quarticity, alpha_series_from_quarticity
-from .tuning import tune_rvdlm_ohlc
-from .dlm_baseline import dlm_lag1_logpred, grid_search_dlm
-from .scoring import log_score_series, bayes_factor_summary  # to be written
+from .dynamic_gamma import DynamicGammaFilter  # or GammaF_Filter if that's the name
+from .rvdlm_univariate import (
+    combined_neg_loglike_univariate_lag1,
+    grid_search_univariate_lag1,
+    # plus anything else you defined there
+)
+from .tuning import tune_rvdlm_ohlc  # if you have it
+from .dlm_baseline import dlm_lag1_logpred, grid_search_dlm  # <-- NEW
 
 __all__ = [
     "__version__",
     "DynamicGammaFilter",
-    "RVDLM_Univariate",
+    "combined_neg_loglike_univariate_lag1",
+    "grid_search_univariate_lag1",
     "tune_rvdlm_ohlc",
     "dlm_lag1_logpred",
     "grid_search_dlm",
